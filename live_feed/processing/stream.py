d9c32edd3eb9c0de3a3f59412bbdddebb4feaddd
mycoal99 <hau.michael1@gmail.com>
2020-11-09 17:21:19 -0800

    rtsp displayed with opencv

--- live_feed/processing/stream.py
+++ live_feed/processing/stream.py
@@ -1,6 +1,16 @@
 import cv2
 import numpy as np
 
-STREAM_LINK = ''
+STREAM_LINK = 'rtsp://192.168.0.28:554/h264'
 
-cap = cv2.VideoCapture(STREAM_LINK)
+cap = cv2.VideoCapture()
+cap.open(STREAM_LINK)
+
+while True:
+	ret, frame = cap.read()
+	window = frame[0:500, 0:500]
+	cv2.imshow('Video',window)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+cap.release()
+cv2.destroyAllWindows()
