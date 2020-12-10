cd6b12c7131d43f2d08f0a8c62328a166bc4d2c0
Abby Wysopal <abbywysopal@ucsb.edu>
2020-12-09 16:06:07 -0800

    create video

--- face-eye recognition/createVideo.py
+++ face-eye recognition/createVideo.py
@@ -3,9 +3,10 @@ from PIL import Image
 import cv2
 import os
 import imageio
+import time
 
 if __name__ == "__main__":
-    write_to = "C:\\Users\\Michael\\Desktop\\video\\EyeDetectionDemo.mp4"
+    write_to = "C:\\Users\\Michael\\Desktop\\video\\EyeDetectionDemo" + str(time.time()) + ".mp4"
     image_folder = 'C:\\Users\\Michael\\Desktop\\video'
     writer = imageio.get_writer(write_to, format='mp4', mode='I', fps=2)
     images = [img for img in os.listdir(image_folder) if (img.endswith('.png') and img.startswith("EyeDectection"))]
@@ -13,4 +14,15 @@ if __name__ == "__main__":
         name = 'C:\\Users\\Michael\\Desktop\\video\\EyeDectection-' + str(i) + '.png'
         img = Image.open(name)
         writer.append_data(np.asarray(img))
-    writer.close()    
+    writer.close()
+
+
+    write_to = "C:\\Users\\Michael\\Desktop\\video\\SimulationDemo" + str(time.time()) + ".mp4"
+    image_folder = 'C:\\Users\\Michael\\Desktop\\video'
+    writer = imageio.get_writer(write_to, format='mp4', mode='I', fps=2)
+    images = [img for img in os.listdir(image_folder) if (img.endswith('.png') and img.startswith("demo"))]
+    for i in range(len(images)):
+        name = 'C:\\Users\\Michael\\Desktop\\video\\demo-' + str(i) + '.png'
+        img = Image.open(name)
+        writer.append_data(np.asarray(img))
+    writer.close()
