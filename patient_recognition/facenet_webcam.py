0ae6260f9a137ebf07928338caacf74bdec95990
Ryan Mitchell <ryanm777@gmail.com>
2020-12-10 17:00:27 -0800

    added green color for longest face in view

--- patient_recognition/facenet_webcam.py
+++ patient_recognition/facenet_webcam.py
@@ -28,77 +28,107 @@ class FaceDetector(object):
         try:
             for box, prob, ld in zip(boxes, probs, landmarks):
                 if debugging:
-                    # Draw rectangle on frame
-                    cv2.rectangle(frame,
-                                (box[0], box[1]),
-                                (box[2], box[3]),
-                                (0, 0, 255),
-                                thickness=2)
-
-                    # Show probability
-                    cv2.putText(frame, str(
-                        prob), (box[2], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-
-                    # Draw landmarks
-                    cv2.circle(frame, tuple(ld[0]), 5, (0, 0, 255), -1)
-                    cv2.circle(frame, tuple(ld[1]), 5, (0, 0, 255), -1)
-                    cv2.circle(frame, tuple(ld[2]), 5, (0, 0, 255), -1)
-                    cv2.circle(frame, tuple(ld[3]), 5, (0, 0, 255), -1)
-                    cv2.circle(frame, tuple(ld[4]), 5, (0, 0, 255), -1)
-
-                # Save Frame State
-                updated = False
-                if len(self.states) != 0:
-                    for state in self.states:
-                        # print("2")
-                        if (((math.isclose(state[0][0], ld[0][0], abs_tol=50)) and (math.isclose(state[0][1], ld[0][1], abs_tol=50)))  or ((math.isclose(state[1][0], ld[1][0], abs_tol=50)) and (math.isclose(state[1][1], ld[1][1], abs_tol=50)))) and (x == state[4]):
-                            state[0] = ld[0]
-                            state[1] = ld[1]
-                            state[3] += 1
-                            state[2] = ((state[2] * (state[3]-1)) + prob) / state[3]
-                            rise = (state[1][1] - state[0][1])
-                            run = (state[1][0] - state[0][0])
-                            if run != 0:
-                                state[5] = math.degrees(math.atan( rise / run )) + (90 * state[4])
-                            else:
-                                if rise > 0:
-                                    state[5] = math.degrees(math.pi / 2) + (90 * state[4])
+                    # Save Frame State'
+                    longestFace = 0
+                    isLongest = False
+                    updated = False
+                    if len(self.states) != 0:
+                        for state in self.states:
+                            # print("2")
+                            if (state[3] > longestFace):
+                                longestFace = state[3]
+                            if (((math.isclose(state[0][0], ld[0][0], abs_tol=50)) and (math.isclose(state[0][1], ld[0][1], abs_tol=50)))  or ((math.isclose(state[1][0], ld[1][0], abs_tol=50)) and (math.isclose(state[1][1], ld[1][1], abs_tol=50)))) and (x == state[4]):
+                                state[0] = ld[0]
+                                state[1] = ld[1]
+                                state[3] += 1
+                                if (state[3] > longestFace):
+                                    isLongest = True
+                                    longestFace = state[3]
+                                state[2] = ((state[2] * (state[3]-1)) + prob) / state[3]
+                                rise = (state[1][1] - state[0][1])
+                                run = (state[1][0] - state[0][0])
+                                if run != 0:
+                                    state[5] = math.degrees(math.atan( rise / run )) + (90 * state[4])
                                 else:
-                                    state[5] = math.degrees((3 * math.pi) / 2) + (90 * state[4])
-                            updated = True
-                            # print("6")
-                        else:
-                            # print("7")
-                            continue
-                else:
-                    # print("3")
-                    rise = (ld[1][1] - ld[0][1])
-                    run = (ld[1][0] - ld[0][0])
-                    if run != 0:
-                        theta = math.atan( rise / run )
+                                    if rise > 0:
+                                        state[5] = math.degrees(math.pi / 2) + (90 * state[4])
+                                    else:
+                                        state[5] = math.degrees((3 * math.pi) / 2) + (90 * state[4])
+                                updated = True
+                                # print("6")
+                            else:
+                                # print("7")
+                                continue
+                            
                     else:
-                        if rise > 0:
-                            theta = math.pi / 2
+                        # print("3")
+                        rise = (ld[1][1] - ld[0][1])
+                        run = (ld[1][0] - ld[0][0])
+                        if run != 0:
+                            theta = math.atan( rise / run )
                         else:
-                            theta = (3 * math.pi) / 2
-                    self.states.append([ld[0], ld[1], prob, 1, x, math.degrees(theta) + (90 * x)])
-                    updated = True
+                            if rise > 0:
+                                theta = math.pi / 2
+                            else:
+                                theta = (3 * math.pi) / 2
+                        self.states.append([ld[0], ld[1], prob, 1, x, math.degrees(theta) + (90 * x)])
+                        updated = True
 
-                if updated:
-                    # print("4")
-                    pass
-                else:
-                    # print("5")
-                    rise = (ld[1][1] - ld[0][1])
-                    run = (ld[1][0] - ld[0][0])
-                    if run != 0:
-                        theta = math.atan( rise / run )
+                    if updated:
+                        # print("4")
+                        pass
                     else:
-                        if rise > 0:
-                            theta = math.pi / 2
+                        # print("5")
+                        rise = (ld[1][1] - ld[0][1])
+                        run = (ld[1][0] - ld[0][0])
+                        if run != 0:
+                            theta = math.atan( rise / run )
                         else:
-                            theta = (3 * math.pi) / 2
-                    self.states.append([ld[0], ld[1], prob, 1, x, math.degrees(theta) + (90 * x)])
+                            if rise > 0:
+                                theta = math.pi / 2
+                            else:
+                                theta = (3 * math.pi) / 2
+                        self.states.append([ld[0], ld[1], prob, 1, x, math.degrees(theta) + (90 * x)])
+
+
+                    # Draw rectangle on frame
+                    if (isLongest == False):
+                        cv2.rectangle(frame,
+                                    (box[0], box[1]),
+                                    (box[2], box[3]),
+                                    (0, 0, 255),
+                                    thickness=2)
+
+                        # Show probability
+                        cv2.putText(frame, str(
+                            prob), (box[2], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+                        # Draw landmarks
+                        cv2.circle(frame, tuple(ld[0]), 5, (0, 0, 255), -1)
+                        cv2.circle(frame, tuple(ld[1]), 5, (0, 0, 255), -1)
+                        cv2.circle(frame, tuple(ld[2]), 5, (0, 0, 255), -1)
+                        cv2.circle(frame, tuple(ld[3]), 5, (0, 0, 255), -1)
+                        cv2.circle(frame, tuple(ld[4]), 5, (0, 0, 255), -1)
+                    
+                    if (isLongest):
+                        cv2.rectangle(frame,
+                                    (box[0], box[1]),
+                                    (box[2], box[3]),
+                                    (0, 255, 0),
+                                    thickness=2)
+
+                        # Show probability
+                        cv2.putText(frame, str(
+                            prob), (box[2], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+                        # Draw landmarks
+                        cv2.circle(frame, tuple(ld[0]), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, tuple(ld[1]), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, tuple(ld[2]), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, tuple(ld[3]), 5, (0, 255, 0), -1)
+                        cv2.circle(frame, tuple(ld[4]), 5, (0, 255, 0), -1)
+
+                
 
         except:
             pass
@@ -165,4 +195,4 @@ class FaceDetector(object):
 if __name__ == "__main__":
     mtcnn = MTCNN()
     fd = FaceDetector(mtcnn)
-    print(fd.start())
+    print(fd.start(debugging=True))
