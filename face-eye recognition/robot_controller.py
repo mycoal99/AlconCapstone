828c7d6e105f1b29c4a99f6e2c03a63724c6d53e
Abby Wysopal <abbywysopal@ucsb.edu>
2021-01-05 15:55:59 -0800

    removing files for PR

--- face-eye recognition/robot_controller.py
+++
@@ -1,9 +0,0 @@
-from facenet_webcam import FaceDetector
-from facenet_webcam import MTCNN
-import sys
-
-if __name__ == "__main__":
-    mtcnn = MTCNN()
-    fd = FaceDetector(mtcnn)
-    patient = fd.start(fd.videoSources["redding"])
-    print(patient)
