a7fb4abc61658fbe79ce890b26b699bc4cd0ae94
Abby Wysopal <abbywysopal@ucsb.edu>
2021-01-20 15:53:40 -0800

    iris detection

--- face-eye recognition/iris_detection.py
+++ face-eye recognition/iris_detection.py
@@ -2,139 +2,22 @@ import numpy as np
 import cv2
 import math
 import string
+import sys
 from scipy import signal
-from .contour_integral_circular import contour_integral_circular
+# from segmentation.segmentiris import segmentiris
+
 '''
     Assumptions:
         - one specified eye is in frame
         - no zooming or cropping of the image
         - only moving the robot arm
 '''
-def contour_integral_circular(imagen, y0, x0, r, angs):
-   """
-   Description:
-       Performs contour circular integral using discrete Rie-mann approach.
- 
-   Input:
-       imagen: The input iris image.
-       y0:     The y-coordinate of the circle center.
-       x0:     The x-coordinate of the circle center.
-       r:      The radius of the circle.
-       angs:   integration angles clockwise 0-2pi.
- 
-   Output:
-       hs:     Hough space result (from integration)
-   """
- 
-   # Get y, x
-   y = np.zeros([len(angs), r.shape[0], r.shape[1], r.shape[2]], dtype=int)
-   x = np.zeros([len(angs), r.shape[0], r.shape[1], r.shape[2]], dtype=int)
-   for i in range(len(angs)):
-       ang = angs[i]
-       y[i, :, :, :] = np.round(y0 - np.cos(ang) * r).astype(int)
-       x[i, :, :, :] = np.round(x0 + np.sin(ang) * r).astype(int)
- 
-   # Adapt y
-   ind = np.where(y < 0)
-   y[ind] = 0
-   ind = np.where(y >= imagen.shape[0])
-   y[ind] = imagen.shape[0] - 1
- 
-   # Adapt x
-   ind = np.where(x < 0)
-   x[ind] = 0
-   ind = np.where(x >= imagen.shape[1])
-   x[ind] = imagen.shape[1] - 1
- 
-   # Return
-   hs = imagen[y, x]
-   hs = np.sum(hs, axis=0)
-   return hs.astype(float)
-
-def search_inner_bound(img):
-   """
-   Description:
-       Search for the inner boundary of the iris.
- 
-   Input:
-       img:    The input iris image.
- 
-   Output:
-       inner_y:    y-coordinate of the inner circle centre.
-       inner_x:    x-coordinate of the inner circle centre.
-       inner_r:    Radius of the inner circle.
-   """
-   # print("---searchInnerBound")
- 
-   # Integro-Differential operator coarse (jump-level precision)
-   Y = img.shape[0]
-   X = img.shape[1]
-   sect = X/4      # Width of the external marg in for which search is excluded
-   minrad = 10
-   maxrad = sect*0.8
-   jump = 4        # Precision of the coarse search, in pixels
- 
-   # Hough Space (y,x,r)
-   sz = np.array([np.floor((Y-2*sect)/jump),
-                   np.floor((X-2*sect)/jump),
-                   np.floor((maxrad-minrad)/jump)]).astype(int)
- 
-   # Resolution of the circular integration
-   integrationprecision = 1
-   angs = np.arange(0, 2*np.pi, integrationprecision)
-   x, y, r = np.meshgrid(np.arange(sz[1]),
-                         np.arange(sz[0]),
-                         np.arange(sz[2]))
-   y = sect + y*jump
-   x = sect + x*jump
-   r = minrad + r*jump
-   hs = contour_integral_circular(img, y, x, r, angs)
- 
-   # Hough Space Partial Derivative R
-   hspdr = hs - hs[:, :, np.insert(np.arange(hs.shape[2]-1), 0, 0)]
- 
-   # Blur
-   sm = 3      # Size of the blurring mask
-   hspdrs = signal.fftconvolve(hspdr, np.ones([sm,sm,sm]), mode="same")
- 
-   indmax = np.argmax(hspdrs.ravel())
-   y,x,r = np.unravel_index(indmax, hspdrs.shape)
- 
- 
-   inner_y = sect + y*jump
-   inner_x = sect + x*jump
-   inner_r = minrad + (r-1)*jump
- 
-   # Integro-Differential operator fine (pixel-level precision)
-   integrationprecision = 0.1      # Resolution of the circular integration
-   angs = np.arange(0, 2*np.pi, integrationprecision)
-   x, y, r = np.meshgrid(np.arange(jump*2),
-                         np.arange(jump*2),
-                         np.arange(jump*2))
-   y = inner_y - jump + y
-   x = inner_x - jump + x
-   r = inner_r - jump + r
-   hs = contour_integral_circular(img, y, x, r, angs)
- 
-   # Hough Space Partial Derivative R
-   hspdr = hs - hs[:, :, np.insert(np.arange(hs.shape[2]-1), 0, 0)]
- 
-   # Bluring
-   sm = 3      # Size of the blurring mask
-   hspdrs = signal.fftconvolve(hspdr, np.ones([sm,sm,sm]), mode="same")
-   indmax = np.argmax(hspdrs.ravel())
-   y,x,r = np.unravel_index(indmax, hspdrs.shape)
- 
-   inner_y = inner_y - jump + y
-   inner_x = inner_x - jump + x
-   inner_r = inner_r - jump + r - 1
-   # print("INNERBOUND:", inner_y, inner_x, inner_r)
-   return inner_y, inner_x, inner_r
-
 
 #set surgical camera as videoSource
 VIDEO_SOURCE = 0
 SET_UP_COMPLETE = False
+EYELASHES_THRESHOLD = 80
+USE_MULTIPROCESSOR = False
 
 #begin live stream
 cap = cv2.VideoCapture(VIDEO_SOURCE) 
@@ -144,17 +27,62 @@ height, width, channels = img.shape
 try:
     while not SET_UP_COMPLETE:
         ret, img = cap.read()
-        rows, cols, _ = img.shape 
+        height, width, channels = img.shape 
 
         #Iris and Pupil detection
-        search_inner_bound(img)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+        gray_blurred = cv2.medianBlur(gray_blurred, 3)
 
-        #move robot to center eye
+        threshold = cv2.threshold(gray_blurred, 15, 255, cv2.THRESH_BINARY_INV)[1]
+        contours = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        #Pupil detection
+        if type(contours[1]) != None:
+            contours = sorted(contours[0], key=lambda x: cv2.contourArea(x), reverse=True)
+            for cnt in contours:
+                (a, b, w, h) = cv2.boundingRect(cnt)
+                cv2.circle(img, (a + int(w/2), b + int(h/2)), int((h)/3), (255, 0, 255), 2)
+                r = int((h)/3)
+            
+                if(len(contours) == 1):
+                    #set pupil in center of camera
+                    #API call- move (north, south, east, west), wait, stop
+                    print(a,b,r)
+
+        #Iris detection
+        #play around with param1 and param2
+        detected_circles = cv2.HoughCircles(gray_blurred,  
+            cv2.HOUGH_GRADIENT, 1, 20, param1 = 100, 
+            param2 = 50, minRadius = 1, maxRadius = int(width/2))
+
+        # find the iris and move closer to eye
+        if detected_circles is not None: 
+            detected_circles = np.uint16(np.around(detected_circles)) 
         
+            for pt in detected_circles[0, :]:
+                a, b, r = pt[0], pt[1], pt[2] 
+                #print(a, b, r)
+                cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+                cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+                #API call- move (up and down), wait, stop
+
+                '''
+                if r >= FINAL_IRIS_SIZE:
+                    SET_UP_COMPLETE = true
+                '''
+
 
         #move robot to closer to eye
+        cv2.imshow('Surgical Camera View', img)
+        k = cv2.waitKey(30) & 0xff
+        
+    cap.release()
+    cv2.destroyAllWindows()
 
 
+except OSError:
+    print(OSError)
 
 
 '''
