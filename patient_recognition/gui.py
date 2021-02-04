b3508ec2262f602512c2e912a738158e9d659ac3
Ryan Mitchell <ryanm777@gmail.com>
2021-02-03 16:12:20 -0800

    added dob label to gui.kv and prepared gui.py for integration with db

--- patient_recognition/gui.py
+++ patient_recognition/gui.py
@@ -17,6 +17,7 @@ class Gui(Widget):
     focus_button = ObjectProperty(None)
     reset_button = ObjectProperty(None)
     name_label = StringProperty()
+    dob_label = StringProperty()
     surgery_label = StringProperty()
     eye_label = StringProperty()
     robot = None
@@ -27,11 +28,13 @@ class Gui(Widget):
         pass
     def focus(self, button):
         # Focuses surgical camera on patient's eye.
+        # GetPatientInfo(whichEyeString, eyeImage)
         pass
     def reset(self, button):
         # Resets Robot back to initial position
         # robot.initial()
         pass
+
 
 class GuiApp(MDApp):
     def build(self):
@@ -41,9 +44,13 @@ class GuiApp(MDApp):
         self.theme_cls.accent_hue = "A200"
         self.theme_cls.theme_style = "Dark"
         gui = Gui()
-        gui.name_label = "Name:  {}".format("Michael Who")
-        gui.surgery_label = "Surgery Type:  {}".format("Cataracts")
-        gui.eye_label = "Eye:  {}".format("Left")
+        # whichEye = Robot.getWhichEye()
+        # eyeImage = Robot.getEyeImage()
+        # patientInfo = getPatientInfo("whichEye", eyeImage)
+        gui.name_label = "Name:  {} {}".format("Michael", "Who") # .format(patientInfo["firstname"], patientInfo["lastname"])
+        gui.dob_label = "Date of Birth: {}".format("11/13/2001") # .format(patientInfo["DOB"])
+        gui.surgery_label = "Surgery Type:  {}".format("Cataracts") # .format(patientInfo["surgery"])
+        gui.eye_label = "Eye:  {}".format("Left") # .format("whichEye")
         return gui
 
 
