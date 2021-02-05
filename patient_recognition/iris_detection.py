05c172e4e764406b314533523a287dbc45f6f27f
mknguyen1202 <mknguyen1202@gmail.com>
2021-02-05 12:58:57 -0800

    robot integration code thus far

--- patient_recognition/iris_detection.py
+++ patient_recognition/iris_detection.py
@@ -18,20 +18,15 @@ from robot_controller import Robot
 
 def center(robot=0, videoSource=0, sleep_time = .5):
     EYE_CENTERED = False
-    WIDTH_THRESHOLD = 10
-    HEIGHT_THESHOLD = 5
+    WIDTH_THRESHOLD = 15
+    HEIGHT_THESHOLD = 10
 
     #begin live stream
     #set surgical camera as videoSource
     cap = cv2.VideoCapture(videoSource) 
     ret, img = cap.read()
     height, width, channels = img.shape
-    counter = 0
     photoNum = 0
-
-    # center()
-    # zoom()
-    # center()
 
     try:
         while not EYE_CENTERED:
@@ -45,19 +40,18 @@ def center(robot=0, videoSource=0, sleep_time = .5):
 
             threshold = cv2.threshold(gray_blurred, 15, 255, cv2.THRESH_BINARY_INV)[1]
             contours = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            path = "C:\\Users\\Alcon\\Desktop\\eyeimages\\"
-            photoName = path + "Center_Eye_1_" + str(photoNum) + ".png"
-            print(photoName)
+            # path = "C:\\Users\\Alcon\\Desktop\\eyeimages\\"
+            # photoName = path + "Center_Eye_1_" + str(photoNum) + ".png"
+            # print(photoName)
             #Pupil detection
             if type(contours[1]) != None:
                 contours = sorted(contours[0], key=lambda x: cv2.contourArea(x))
-                average_a = []
-                average_b = []
-                index = 0 
+                average_a = [0]*len(contours)
+                average_b = [0]*len(contours)
+                index = 0
                 for cnt in contours:
                     (a, b, w, h) = cv2.boundingRect(cnt)
                     cv2.circle(img, (a + int(w/2), b + int(h/2)), int((h)/3), (255, 0, 255), 2)
-                    print("counters: ", (int((h)/3)))
                     average_a[index] = a + int(w/2)
                     average_b[index] = b + int(h/2)
                     index += 1
@@ -67,36 +61,42 @@ def center(robot=0, videoSource=0, sleep_time = .5):
                 #     print("a: ", a, ", b: ", b, ", w: ", w, ", h: ", h)
                     #set pupil in center of camera
                     #API call- move (north, south, east, west), wait, stop
-                if(sum(average_a) > int(width/2) - WIDTH_THRESHOLD and sum(average_a) < int(width/2) + WIDTH_THRESHOLD
-                    and sum(average_b) < int(height/2) + HEIGHT_THESHOLD and sum(average_b) > int(height/2) - HEIGHT_THESHOLD
-                ):
+                
+                print()
+                sum_a = sum(average_a)/index
+                sum_b = sum(average_b)/index
+                # print("sum_a: ", sum_a)
+                # print("sum_b: ", sum_b)
+                if(sum_a > int(width/2) - WIDTH_THRESHOLD and sum_a < int(width/2) + WIDTH_THRESHOLD 
+                    and sum_b < int(height/2) + HEIGHT_THESHOLD 
+                    and sum_b > int(height/2) - HEIGHT_THESHOLD):
                     EYE_CENTERED = True
 
-                if(sum(average_a) < int(width/2) - WIDTH_THRESHOLD):
+                if(sum_a < int(width/2) - WIDTH_THRESHOLD):
                     print("move right")
                     robot.right()
                     time.sleep(sleep_time)
                     robot.stop()
 
-                if(sum(average_a) > int(width/2) + WIDTH_THRESHOLD):
+                if(sum_a > int(width/2) + WIDTH_THRESHOLD):
                     print("move left")
                     robot.left()
                     time.sleep(sleep_time)
                     robot.stop()
                     
-                if(sum(average_b) > int(height/2) + HEIGHT_THESHOLD):
+                if(sum_b > int(height/2) + HEIGHT_THESHOLD):
                     print("move forward")
                     robot.forward()
                     time.sleep(sleep_time)
                     robot.stop()
 
-                if(sum(average_b) < int(height/2) - HEIGHT_THESHOLD):
+                if(sum_b < int(height/2) - HEIGHT_THESHOLD):
                     print("move backward")    
                     robot.backward()
                     time.sleep(sleep_time)
                     robot.stop()
 
-            cv2.imwrite(photoName, img)
+            # cv2.imwrite(photoName, img)
             photoNum += 1
             cv2.imshow("camera", img)
             cv2.waitKey(10)
@@ -110,9 +110,9 @@ def center(robot=0, videoSource=0, sleep_time = .5):
         print("error Center Function", OSError)
 
 
-def zoom(robot=0, videoSource=0):
+def zoom(robot=0, videoSource=0, sleep_time = .2):
     SET_UP_COMPLETE = False
-    FINAL_IRIS_SIZE = 35 #TODO
+    FINAL_IRIS_SIZE = 95 #TODO
 
     #begin live stream
     #set surgical camera as videoSource
@@ -140,7 +140,7 @@ def zoom(robot=0, videoSource=0):
             #play around with param1 and param2
             detected_circles = cv2.HoughCircles(gray_blurred,  
                 cv2.HOUGH_GRADIENT, 1, 20, param1 = 40, 
-                param2 = 20, minRadius = 5, maxRadius = 50)
+                param2 = 20, minRadius = 50, maxRadius = 120)
 
             # find the iris and move closer to eye
             if detected_circles is not None: 
@@ -157,17 +157,19 @@ def zoom(robot=0, videoSource=0):
                         cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
                         #API call- move (up and down), wait, stop
 
-                        if(r < FINAL_IRIS_SIZE):
+                        if(r < FINAL_IRIS_SIZE or counter < 10):
                             print("move down")
-                            robot.down()
-                            time.sleep(.5)
-                            robot.stop()
+                            # robot.down()
+                            # time.sleep(sleep_time)
+                            # robot.stop()
                             counter += 1
 
-                        if (r >= FINAL_IRIS_SIZE):
+                        if (r >= FINAL_IRIS_SIZE or counter >= 10):
                             print("setup complete")
                             cv2.imwrite("Lastmoment.png", img)
+                            cv2.imwrite(photoName, img)
                             SET_UP_COMPLETE = True
+                            break
 
             #move robot to closer to eye
             cv2.imwrite(photoName, img)
@@ -185,9 +187,9 @@ def zoom(robot=0, videoSource=0):
 
 
 def moveRobotToEye(robot=0, videoSource=0):
-    center(robot, videoSource)
+    # center(robot, videoSource, .4)
     zoom(robot, videoSource)
-    center(robot, videoSource, .3)
+    # center(robot, videoSource, .2)
 
 
 '''
@@ -204,5 +206,22 @@ if __name__ == "__main__":
         # seperate center and zoom functionality
         # change sleep parameters
         # call center then zoom then center
-        # decrease range of ertror for centering
+        # decrease range of error for centering
+
+    # cap = cv2.VideoCapture(0)
+
+    # while(True):
+    #     # Capture frame-by-frame
+    #     ret, frame = cap.read()
+
+    #     # Our operations on the frame come here
+
+    #     # Display the resulting frame
+    #     cv2.imshow('frame',frame[::-1])
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+
+    # # When everything done, release the capture
+    # cap.release()
+    # cv2.destroyAllWindows()
 
--- patient_recognition/robot_controller.py
+++ patient_recognition/robot_controller.py
@@ -86,188 +86,245 @@ from CapstoneClient import CapstoneClient
 def calc2DDistance(x1,y1,x2,y2):
     return math.sqrt(abs(x2-x1)**2 + abs(y2-y1)**2)
 
-def moveRobotToPatient(robot=0, left=False, patient=0, videoSource=0):
-    # Assign coordinates of QRCode to Robot object
-    robot.updateCoords(videoSource)
-    # Get coordinates of patient's correct eye
-    if (left):
-        eyeX = patient[0][0]
-        eyeY = patient[0][1]
-    else:
-        eyeX = patient[1][0]
-        eyeY = patient[1][1]
+def moveRobotToPatient(robot=0, left=False, patient=[[]], videoSource=0):
+		# Assign coordinates of QRCode to Robot object\
+		print(videoSource)
+		robot.updateCoords(videoSource)
 
-    # Calculate the distance between the robot and the eye
-    # Instantiate the var that will update as the robot is moving that will determine if the stop command is sent.
-    distanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-    newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
+		# Get coordinates of patient's correct eye
+		#potentially the wrong eye
+		if (left):
+			eyeX = patient[0][0]
+			eyeY = patient[0][1]
+		else:
+			eyeX = patient[1][0]
+			eyeY = patient[1][1]
 
-    # Repeat this block for each direction twice, to make sure each direction gets atleast one chance to move the robot closer.
-    # Robot moves in one direction, then consistently updates its coordinates and its distance to the eye.
-    # If the new distance to the eye is greater than the previous difference, the loop breaks and the robot stops
-    # If the distance to the eye is less than 15 pixels, the function returns
-    # Once the robot stops, the newDistance is set to the baseline distance, and the robot begins again in a new direction until it returns.
-    robot.left()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		# Calculate the distance between the robot and the eye
+		# Instantiate the var that will update as the robot is moving that will determine if the stop command is sent.
+		distanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
+		newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
 
-    robot.forward()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		# Repeat this block for each direction twice, to make sure each direction gets atleast one chance to move the robot closer.
+		# Robot moves in one direction, then consistently updates its coordinates and its distance to the eye.
+		# If the new distance to the eye is greater than the previous difference, the loop breaks and the robot stops
+		# If the distance to the eye is less than 15 pixels, the function returns
+		# Once the robot stops, the newDistance is set to the baseline distance, and the robot begins again in a new direction until it returns.
+		
+		sleep_time = 2.5
+		final_distance_to_eye = 10
 
-    robot.right()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		while newDistanceToEye <= distanceToEye:
+			print("right while loop 1")
+			robot.right()
+			time.sleep(sleep_time)
+			robot.stop()
+			distanceToEye = newDistanceToEye
+			robot.updateCoords(videoSource)
+			newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
+			if newDistanceToEye < final_distance_to_eye:
+				return True
+		distanceToEye = newDistanceToEye
 
-    robot.backward()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		while newDistanceToEye <= distanceToEye:
+			print("forward while loop 1")
+			robot.forward()
+			time.sleep(sleep_time)
+			robot.stop()
+			distanceToEye = newDistanceToEye
+			robot.updateCoords(videoSource)
+			newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
+			if newDistanceToEye < final_distance_to_eye:
+				return True
+		distanceToEye = newDistanceToEye
 
-    robot.left()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		while newDistanceToEye <= distanceToEye:
+			print("left while loop 1")
+			robot.left()
+			time.sleep(sleep_time)
+			robot.stop()
+			distanceToEye = newDistanceToEye
+			robot.updateCoords(videoSource)
+			newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
+			if newDistanceToEye < final_distance_to_eye:
+				return True
+		distanceToEye = newDistanceToEye
 
-    robot.forward()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		while newDistanceToEye <= distanceToEye:
+			print("backward while loop 1")
+			robot.backward()
+			time.sleep(sleep_time)
+			robot.stop()
+			distanceToEye = newDistanceToEye
+			robot.updateCoords(videoSource)
+			newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
+			if newDistanceToEye < final_distance_to_eye:
+				return True
+		distanceToEye = newDistanceToEye
 
-    robot.right()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True
-    robot.stop()
-    distanceToEye = newDistanceToEye
+		return False
 
-    robot.backward()
-    while newDistanceToEye <= distanceToEye:
-        distanceToEye = newDistanceToEye
-        robot.updateCoords(videoSource)
-        newDistanceToEye = calc2DDistance(robot.getX(),robot.getY(),eyeX,eyeY)
-        if newDistanceToEye < 15:
-            return True 
-    robot.stop()
-    distanceToEye = newDistanceToEye
-    return False
-
-def getPatient():
+def getPatient(source):
     mtcnn = MTCNN()
     fd = FaceDetector(mtcnn)
-    return fd.start(1, False)
+    cap = cv2.VideoCapture(1)
+    ret, frame = cap.read()
+    maxImageHeight = frame.shape[0]
+    maxImageWidth = frame.shape[1]
+    cap.release()
+    patient = fd.start(fd.videoSources[source], False)
+    orientation = patient[4]
+    if orientation == 0:
+        pass
+    elif orientation == 1:
+        tempy1 = patient[0][1]
+        tempy2 = patient[1][1]
+        patient[0][1] = maxImageHeight - patient[0][0]
+        patient[0][0] = tempy1
+        patient[1][1] = maxImageHeight - patient[1][0]
+        patient[1][0] = tempy2
+    elif orientation == 2:
+        patient[0][0] = maxImageWidth - patient[0][0]
+        patient[0][1] = maxImageHeight - patient[0][1]
+        patient[1][0] = maxImageWidth - patient[1][0]
+        patient[1][1] = maxImageHeight - patient[1][1]
+    elif orientation == 3:
+        tempx1 = patient[0][0]
+        tempx2 = patient[1][0]
+        patient[0][0] = maxImageWidth - patient[0][1]
+        patient[0][1] = tempx1
+        patient[1][0] = maxImageWidth - patient[1][1]
+        patient[1][1] = tempx2
+    return patient
 
 class Robot(object):
 
-    # __xCoordinate = 0
-    # __yCoordinate = 0
-    # __controller = CapstoneClient()
+	# __xCoordinate = 0
+	# __yCoordinate = 0
+	# __controller = CapstoneClient()
 
-    def __init__(self):
-        self.__xCoordinate = 0
-        self.__yCoordinate = 0
-        self.__controller = CapstoneClient()
-        self.__controller.start()
-        self.__controller.sendRobotCommand(self.__controller.commands["initial"])
+	def __init__(self):
+		self.__xCoordinate = 0
+		self.__yCoordinate = 0
+		self.__controller = CapstoneClient()
+		self.__controller.start()
+		#self.__controller.sendRobotCommand(self.__controller.commands["initial"])
 
-    def updateCoords(self, videoSource):
-        cap = cv2.VideoCapture(videoSource)
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        qrCode = pyzbar.decode(gray)
-        cap.release()
+	def updateCoords(self, videoSource):
+		qrCode = []
 
-        (topLeft,topRight,bottomRight,bottomLeft) = qrCode[0].polygon
-        centerX = (topLeft.x + bottomRight.x) / 2
-        centerY = (topLeft.y + bottomRight.y) / 2
-        robot.setX(centerX)
-        robot.setY(centerY)
+		while(not qrCode):
+			qrCode = Robot.detectQRCode(videoSource)
 
-    def setX(self, x):
-        self.__xCoordinate = x
+		print(qrCode)
 
-    def setY(self, y):
-        self.__yCoordinate = y
+		# cap = cv2.VideoCapture(videoSource)
+		# FOUND_QR = False
+		# while(not FOUND_QR):
+		# # while(len(qrCode) == 0):		
+		# 	ret, frame = cap.read()
+			
+		# 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# 	#gray[gray>150] = 255
+		# 	cv2.imshow('frame',gray)
+		# 	qrCode = pyzbar.decode(gray)
+		# 	if cv2.waitKey(1) & 0xFF == ord('q'):
+		# 		break
+		# 	if(len(qrCode) > 0):
+		# 		print("zbar:", qrCode)
+		# 		print(len(qrCode[0].polygon))
+		# 		if(len(qrCode[0].polygon) == 4):
+		# 			FOUND_QR = True
 
-    def getX(self):
-        return self.__xCoordinate
+		# cv2.destroyAllWindows()
+		# cap.release()
 
-    def getY(self):
-        return self.__yCoordinate
+		(topLeft,topRight,bottomRight,bottomLeft) = qrCode[0].polygon
+		centerX = (topLeft.x + bottomRight.x) / 2
+		centerY = (topLeft.y + bottomRight.y) / 2
+		robot.setX(centerX)
+		robot.setY(centerY)
 
-    def right(self):
-        self.__controller.sendRobotCommand(self.__controller.commands["right"])
-    def left(self):
-        self.__controller.sendRobotCommand(self.__controller.commands["left"])
-    def forward(self):
-        self.__controller.sendRobotCommand(self.__controller.commands["forward"])
-    def backward(self):
-        self.__controller.sendRobotCommand(self.__controller.commands["backward"])
-    def up(self):
-        self.__controller.sendRobotCommand(self.__controller.commands["up"])
-    def down(self):
-        self.__controller.sendRobotCommand(self.__controller.commands["down"])
-    def stop(self):
-        __controller.sendRobotCommand(__controller.commands["stop"])
-    def initial(self):
-        __controller.sendRobotCommand(__controller.commands["initial"])
+	def detectQRCode(videoSource):
+		cap = cv2.VideoCapture(videoSource)
+		cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+		cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+		ret, frame = cap.read()
+		cap.release()
+
+		print(frame.shape)
+		blurFrame = cv2.GaussianBlur(frame, (21,21), 5)
+		sharpFrame = cv2.addWeighted(frame, 1.5, blurFrame, -0.5, 0)
+		qrCode2 = []
+		qrCode = pyzbar.decode(sharpFrame)
+
+		return qrCode2
+
+	def setX(self, x):
+		self.__xCoordinate = x
+
+	def setY(self, y):
+		self.__yCoordinate = y
+
+	def getX(self):
+		return self.__xCoordinate
+
+	def getY(self):
+		return self.__yCoordinate
+		
+	def initial(self):
+		print("initial")
+		self.__controller.sendRobotCommand(self.__controller.commands["initial"])
+	def right(self):
+		print("right")
+		self.__controller.sendRobotCommand(self.__controller.commands["right"])
+	def left(self):
+		print("left")
+		self.__controller.sendRobotCommand(self.__controller.commands["left"])
+	def forward(self):
+		print("forward")
+		self.__controller.sendRobotCommand(self.__controller.commands["forward"])
+	def backward(self):
+		print("backward")
+		self.__controller.sendRobotCommand(self.__controller.commands["backward"])
+	def up(self):
+		print("up")
+		self.__controller.sendRobotCommand(self.__controller.commands["up"])
+	def down(self):
+		print("down")
+		self.__controller.sendRobotCommand(self.__controller.commands["down"])
+	def stop(self):
+		print("stop")
+		self.__controller.sendRobotCommand(self.__controller.commands["stop"])
 
 
-    
-    def rotate(self, theta):
-        print("Rotating {0} degrees".format(theta))
+	def rotate(self, theta):
+		print("Rotating {0} degrees".format(theta))
 
-    def rotate180(self):
-        print("Rotating 180 Degrees")
+	def rotate180(self):
+		print("Rotating 180 Degrees")
 
-    def rotate90CW(self):
-        print("Rotating 90 Degrees Clockwise")
+	def rotate90CW(self):
+		print("Rotating 90 Degrees Clockwise")
 
-    def rotate90CCW(self):
-        print("Rotating 90 Degrees Counter-Clockwise")
+	def rotate90CCW(self):
+		print("Rotating 90 Degrees Counter-Clockwise")
 
 if __name__ == "__main__":
-    mtcnn = MTCNN()
-    fd = FaceDetector(mtcnn)
-    patient = fd.start(fd.videoSources[sys.argv[1]], False)
-    robot = Robot()
-    print(moveRobotToPatient(robot=robot, left=False, patient=patient, videoSource=fd.videoSources[sys.argv[1]]))
-    print(robot.getX())
-    print(robot.getY())
-    print(patient)
+	# mtcnn = MTCNN()
+	# print("after mtcnn")
+	# fd = FaceDetector(mtcnn)
+	# print("after facedetector")
+	# patient = fd.start(fd.videoSources[sys.argv[1]], False)
+	# print("after fd start")
+	robot = Robot()
+	robot.initial()
+	time.sleep(3)
+	patient = getPatient(sys.argv[1])
+	print("after robot")
+	moveRobotToPatient(robot=robot, left=False, patient=patient, videoSource=1)
+	print(robot.getX())
+	print(robot.getY())
+	print(patient)
+
+	#change sleep_time based on distance
